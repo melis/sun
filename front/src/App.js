c1c4cc64ac41f21bdbafe850a7de569a4d3cd186
Melis <59087686+melis@users.noreply.github.com>
2022-08-13 20:30:54 +0600

    r

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Card from "./components/Card/Card";
-
+import axios from "axios";
 import Form from "./components/Form/Form";
 import { getPokemons } from "./store/pokemons/pokemonsAction";
 
@@ -18,6 +18,15 @@ function App() {
 
   return (
     <div className="App container">
+      <button
+        onClick={() => {
+          axios
+            .get("http://localhost:5000/api/pokemons")
+            .then(({ data }) => console.log(data));
+        }}
+      >
+        TEst
+      </button>
       {pokemons.map((el) => (
         <Card
           id={el._id}
